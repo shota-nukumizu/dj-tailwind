7812316358f9abd32a55d9fd0f1d16f08d93aba6
shota-nukumizu <author2000.1225@gmail.com>
2022-01-17 08:06:06 +0900

    create Model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    is_done = models.BooleanField(default=False)
