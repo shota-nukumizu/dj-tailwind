919f9d3279ed13160471c3a89720ee34a9ab1ecd
shota-nukumizu <author2000.1225@gmail.com>
2022-01-17 08:12:53 +0900

    create view for adding tasks

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import todos
+from todo.views import todos, add_todo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todos, name='todo'),
+    path('add-todo/', add_todo, name='add_todo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from .models import Todo
 
 def todos(request):
     return render(request, 'todos.html')
+
+@require_http_methods(['POST'])
+def add_todo(request):
+    todo = None
+    title = request.POST.get('title', '')
+
+    if title:
+        todo = Todo.objects.create(title=title)
+    
+    return render(request, 'partials/todo.html', {'todo': todo})
